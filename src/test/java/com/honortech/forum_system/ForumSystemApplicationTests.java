b56e6ddf7ac085169defe4eba738623249ce04d0
xxneverstop <2954440741@qq.com>
2025-10-21 14:10:46 +0800

    test commit

---
+++ src/test/java/com/honortech/forum_system/ForumSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.honortech.forum_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
